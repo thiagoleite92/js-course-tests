90c3e1d05efdef35e5cabe1c2f2683e53f2546df
Thiago Leite <leite.tjs@gmail.com>
2022-04-26 01:04:38 -0300

    feat: modulo 2 - aula 17 done'

--- module_2/pages/ProductList.integragation.spec.js
+++ module_2/pages/ProductList.integragation.spec.js
@@ -1,9 +1,26 @@
+import Vue from 'vue';
 import { mount } from '@vue/test-utils';
+import axios from 'axios';
 import ProductList from '.';
 import ProductCard from '@/components/ProductCard';
 import Search from '@/components/Search';
+import { makeServer } from '@/miragejs/server';
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+}));
 
 describe('ProductList - integration', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should mount the component', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.vm).toBeDefined();
@@ -15,8 +32,29 @@ describe('ProductList - integration', () => {
     expect(wrapper.findComponent(Search)).toBeDefined();
   });
 
-  it('should mount the ProductCart component 10 times', () => {
-    const wrapper = mount(ProductList);
+  it('should call axios.get on component mount', () => {
+    mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith('/api/products');
+  });
+
+  it('should mount the ProductCart component 10 times', async () => {
+    const products = server.createList('product', 10);
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
 
     const cards = wrapper.findAllComponents(ProductCard);
 
