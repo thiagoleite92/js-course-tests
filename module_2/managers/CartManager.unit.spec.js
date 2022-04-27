0295205662bf6f742d72dd15359ce5401b22c041
Thiago <leite.tjs@gmail.com>
2022-04-27 17:36:06 -0300

    modulo 2 - aula 35

--- module_2/managers/CartManager.unit.spec.js
+++ module_2/managers/CartManager.unit.spec.js
@@ -14,6 +14,20 @@ describe('CartManager', () => {
     server.shutdown();
   });
 
+  it('should return the state', () => {
+    const product = server.create('product');
+
+    manager.open();
+    manager.addProduct(product);
+
+    const state = manager.getState();
+
+    expect(state).toEqual({
+      items: [product],
+      open: true,
+    });
+  });
+
   it('should set cart to open', () => {
     const state = manager.open();
 
