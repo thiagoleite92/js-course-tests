d82098e43a0925e85e277a449aed70cd297a9a53
Thiago <leite.tjs@gmail.com>
2022-04-27 17:29:36 -0300

    modulo 2 - aula 34

--- module_2/managers/CartManager.unit.spec.js
+++ module_2/managers/CartManager.unit.spec.js
@@ -34,5 +34,53 @@ describe('CartManager', () => {
     expect(state.items).toHaveLength(1);
   });
 
-  it('should ', () => {});
+  it('should remove product from the cart', () => {
+    const product = server.create('product');
+    const state = manager.removeProduct(product.id);
+
+    expect(state.items).toHaveLength(0);
+  });
+
+  it('should clear products', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+
+    const state = manager.clearProducts();
+
+    expect(state.items).toHaveLength(0);
+  });
+
+  it('should clear cart', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+    manager.open();
+
+    const state = manager.clearCart();
+
+    expect(state.items).toHaveLength(0);
+    expect(state.open).toBe(false);
+  });
+
+  it('should return true if cart is not empty', () => {
+    const product1 = server.create('product');
+    const product2 = server.create('product');
+
+    manager.addProduct(product1);
+    manager.addProduct(product2);
+
+    expect(manager.hasProducts()).toBe(true);
+  });
+
+  it('should return true if product is already in the cart', () => {
+    const product = server.create('product');
+    manager.addProduct(product);
+
+    expect(manager.productIsInTheCart(product)).toBe(true);
+  });
 });
