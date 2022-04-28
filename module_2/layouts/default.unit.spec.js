518c674c382197fecf8f876589505f9afa2773be
Thiago <leite.tjs@gmail.com>
2022-04-27 21:08:34 -0300

    modulo 2 - aula 38

---
+++ module_2/layouts/default.unit.spec.js
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils';
+import DefaultLayout from '@/layouts/default';
+import Cart from '@/components/Cart';
+import { CartManager } from '@/managers/CartManager';
+
+describe('DefaultLayout', () => {
+  const mountLayout = () => {
+    const wrapper = mount(DefaultLayout, {
+      mocks: {
+        $cart: new CartManager(),
+      },
+      stubs: {
+        Nuxt: true,
+      },
+    });
+
+    return { wrapper };
+  };
+
+  it('should mount cart', async () => {
+    const { wrapper } = mountLayout();
+
+    expect(wrapper.findComponent(Cart).exists()).toBe(true);
+  });
+
+  it('should toggle Cart visibility', async () => {
+    const { wrapper } = mountLayout();
+
+    const button = wrapper.find('[data-testid="toggle-button"]');
+
+    await button.trigger('click');
+    expect(wrapper.vm.isCartOpen).toBe(true);
+    await button.trigger('click');
+    expect(wrapper.vm.isCartOpen).toBe(false);
+  });
+});
