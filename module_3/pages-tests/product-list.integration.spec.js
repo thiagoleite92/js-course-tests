e37fe332c33cc702752fc6571a362cff0d7f79d7
Thiago <leite.tjs@gmail.com>
2022-05-19 14:46:33 -0300

    modulo 3 - aula 16

--- module_3/pages-tests/product-list.integration.spec.js
+++ module_3/pages-tests/product-list.integration.spec.js
@@ -1,6 +1,7 @@
 import { screen, render, waitFor } from '@testing-library/react';
 import ProductList from '../pages';
 import { makeServer } from '../miragejs/server';
+import { Response } from 'miragejs';
 
 const renderProductList = () => {
   render(<ProductList />);
@@ -32,4 +33,28 @@ describe('ProductList', () => {
       expect(screen.getAllByTestId('product-card')).toHaveLength(10);
     });
   });
+
+  it('should render the "no products", message', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('no-products')).toBeInTheDocument();
+    });
+  });
+
+  it('should display error message when promise rejects', async () => {
+    server.get('products', () => {
+      return new Response(500, {}, '');
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('server-error')).toBeInTheDocument();
+      expect(screen.queryByTestId('no-products')).toBeNull();
+      expect(screen.queryAllByTestId('product-card')).toHaveLength(0);
+    });
+  });
+
+  xit('should render the search component', () => {});
 });
