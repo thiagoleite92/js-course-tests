a57447849aa2a1074130e3e6d650b82e85d91742
Thiago <leite.tjs@gmail.com>
2022-05-19 14:05:39 -0300

    module 3 - aula 1ula 14

--- module_3/pages-tests/product-list.integration.spec.js
+++ module_3/pages-tests/product-list.integration.spec.js
@@ -1,10 +1,35 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
 import ProductList from '../pages';
+import { makeServer } from '../miragejs/server';
+
+const renderProductList = () => {
+  render(<ProductList />);
+};
 
 describe('ProductList', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should render Product List', () => {
-    render(<ProductList />);
+    renderProductList();
 
     expect(screen.getByTestId('product-list')).toBeInTheDocument();
+  });
+
+  it('should render the ProductCard component 10 times', async () => {
+    server.createList('product', 10);
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(10);
+    });
   });
 });
