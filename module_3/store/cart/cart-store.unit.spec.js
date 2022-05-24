7dfa05ba07afd9bf62b687664ca1e86ddd07e77a
Thiago <leite.tjs@gmail.com>
2022-05-23 22:15:18 -0300

    modulo 3 - aula 28

--- module_3/store/cart/cart-store.unit.spec.js
+++ module_3/store/cart/cart-store.unit.spec.js
@@ -39,8 +39,18 @@ describe('Cart Store', () => {
     expect(result.current.state.products).toHaveLength(2);
   });
 
+  it('should not add same product twice', () => {
+    const product = server.create('product');
+
+    act(() => add(product));
+    act(() => add(product));
+
+    expect(result.current.state.products).toHaveLength(1);
+  });
+
   it('should toggle open state', async () => {
     expect(result.current.state.open).toBe(false);
+    expect(result.current.state.products).toHaveLength(0);
 
     act(() => toggle());
     expect(result.current.state.open).toBe(true);
@@ -48,5 +58,6 @@ describe('Cart Store', () => {
     act(() => toggle());
 
     expect(result.current.state.open).toBe(false);
+    expect(result.current.state.products).toHaveLength(0);
   });
 });
